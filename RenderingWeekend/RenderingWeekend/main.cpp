ba049000366969c4c7f6c1192ecba627e2b3914a
LeoDarcy <baidurnt@163.com>
2021-04-10 13:04:33 +0800

    week 10

--- RenderingWeekend/RenderingWeekend/main.cpp
+++ RenderingWeekend/RenderingWeekend/main.cpp
@@ -58,13 +58,15 @@ int main() {
 	//world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-	auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
-	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+	//auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+	//auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+	auto material_center = make_shared<dielectric>(1.5);
+	auto material_left = make_shared<dielectric>(1.5);
+	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
 	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
 	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
 	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 
@@ -77,8 +79,7 @@ int main() {
 	auto horizontal = vec3(viewport_width, 0, 0);
 	auto vertical = vec3(0, viewport_height, 0);
 	auto low_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - vec3(0, 0, focal_length);
-	camera cam;
-
+	camera cam(90.0, aspect_ratio);
 
 	//Render
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
